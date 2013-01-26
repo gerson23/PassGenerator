3d5f6cfedf47d388cc3f98a78e9a1834a5d6bb60
Julio Silva <julio@juliobs.com>
2013-01-26 01:52:31 -0200

    Indentation and style following PEP8

--- src/PassGenerator.py
+++ src/PassGenerator.py
@@ -11,59 +11,59 @@ import random
 
 class Password:
 
-  ''' Class constructor '''
-  def __init__(self,size, chars, numbers, others):
+    ''' Class constructor '''
+    def __init__(self, size, chars, numbers, others):
 
-    self.size = size
-    self.chars = chars
-    self.numbers = numbers
-    self.others = others
+        self.size = size
+        self.chars = chars
+        self.numbers = numbers
+        self.others = others
 
-  ''' Returns a random char '''
-  def select_char(self):
-    if(not self.chars):
-      return
+    ''' Returns a random char '''
+    def select_char(self):
+        if(not self.chars):
+            return
 
-    is_caps = random.randint(0,1)
+        is_caps = random.randint(0, 1)
 
-    if(is_caps):
-      return chr(random.randint(65,90))
-    else:
-      return chr(random.randint(97,122))
+        if(is_caps):
+            return chr(random.randint(65, 90))
+        else:
+            return chr(random.randint(97, 122))
 
-  ''' Return a random number converted to char '''
-  def select_num(self):
-    if(not self.numbers):
-      return
+    ''' Return a random number converted to char '''
+    def select_num(self):
+        if(not self.numbers):
+            return
 
-    return chr(random.randint(48,57))
+        return chr(random.randint(48, 57))
 
-  ''' Return an alternate character '''
-  def select_others(self):
-    if(not self.others):
-      return
+    ''' Return an alternate character '''
+    def select_others(self):
+        if(not self.others):
+            return
 
-    return random.choice('@#!&.')
+        return random.choice('@#!&.')
 
-  ''' Chooses type of next character and returns it '''
-  def select_next(self):
-    opt = random.randint(0,4)
+    ''' Chooses type of next character and returns it '''
+    def select_next(self):
+        opt = random.randint(0, 4)
 
-    if opt == 3:
-      return self.select_others()
-    elif opt == 2:
-      return self.select_num()
-    else:
-      return self.select_char()
+        if opt == 3:
+            return self.select_others()
+        elif opt == 2:
+            return self.select_num()
+        else:
+            return self.select_char()
 
-  ''' Create password given the parameters '''
-  def get_pass(self):
-    psw_ = [];
+    ''' Create password given the parameters '''
+    def get_pass(self):
+        psw_ = []
 
-    for i in range(self.size):
-      psw_.append(self.select_next())
+        for i in range(self.size):
+            psw_.append(self.select_next())
 
-    return ''.join(psw_)
+        return ''.join(psw_)
 
 
 if __name__ == "__main__":
